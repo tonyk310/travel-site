ad19b8deb79420cfeef9ae7498575678e42372c5
Mac <mac@Tones-MacBook-Pro.local>
2019-04-16 14:45:13 -0700

    Completed the reveal on scroll behavior

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -5,5 +5,12 @@ import MobileMenu from './modules/mobileMenu.js';
 // simply importing this file will not do anything
 // we need to create a new object that uses this class as a blueprint
 
+import RevealOnScroll from './modules/revealOnScroll.js';
+
+import $ from "jquery";
+
 var mobileMenu = new MobileMenu();
 
+// var revealOnScroll = new RevealOnScroll();
+ new RevealOnScroll($(".feature-item"), "85%");
+ new RevealOnScroll($(".testimonial"), "60%");
