128a10e04eedf222a07244f56feb450986a16882
Mac <mac@Tones-MacBook-Pro.local>
2019-04-17 16:51:54 -0700

    Completed the modal overlay.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -8,6 +8,7 @@ import MobileMenu from './modules/mobileMenu.js';
 import RevealOnScroll from './modules/revealOnScroll.js';
 import $ from "jquery";
 import StickyHeader from './modules/StickyHeader.js';
+import Modal from './modules/Modal.js';
 
 var mobileMenu = new MobileMenu();
 
@@ -16,3 +17,5 @@ new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 
 var stickyHeader = new StickyHeader();
+
+var modal = new Modal();
