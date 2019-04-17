49f201d201c4a683fb363c8e2c68ccd84aef8cf3
Mac <mac@Tones-MacBook-Pro.local>
2019-04-17 09:14:21 -0700

    Completed sticky header functionality

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -6,11 +6,13 @@ import MobileMenu from './modules/mobileMenu.js';
 // we need to create a new object that uses this class as a blueprint
 
 import RevealOnScroll from './modules/revealOnScroll.js';
-
 import $ from "jquery";
+import StickyHeader from './modules/StickyHeader.js';
 
 var mobileMenu = new MobileMenu();
 
 // var revealOnScroll = new RevealOnScroll();
- new RevealOnScroll($(".feature-item"), "85%");
- new RevealOnScroll($(".testimonial"), "60%");
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
+
+var stickyHeader = new StickyHeader();
