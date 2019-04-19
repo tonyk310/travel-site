db1129fc378f707aef01235b2acba1026ce4f7fc
Mac <mac@Tones-MacBook-Pro.local>
2019-04-18 17:01:21 -0700

    Completed gulp build task.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ require('./gulp/tasks/watch.js');
 require('./gulp/tasks/sprites.js');
 require('./gulp/tasks/scripts.js');
 require('./gulp/tasks/modernizr.js');
+require('./gulp/tasks/build.js');
 
 // var gulp = require('gulp');
 // var watch = require('gulp-watch');
